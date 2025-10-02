stuart-cls/tofu

commit cf9a6e4dc5c86882c9e9dfd1fa41f5d280e7fed4

ez: ez-stitch return if user requests to keep data intact

tofu/ez/GUI/Stitch_tools_tab/ezstitch_qt.py
@@ -593,17 +593,21 @@ class EZStitchGroup(QGroupBox):
                     rmtree(dir_path)
                 except:
                     warning_message(f"Error while deleting {dir_type} directory")
-                    return
+                    return False
+                else:
+                    return True
             else:
-                return
+                return False
 
 
 
     def stitch_button_pressed(self):
         LOG.debug("Stitch button pressed")
 
-        self.verify_safe2delete(EZVARS_aux['vert-sti']['tmp-dir']['value'], "Temporary")
-        self.verify_safe2delete(EZVARS_aux['vert-sti']['output-dir']['value'], "Output")
+        if not self.verify_safe2delete(EZVARS_aux['vert-sti']['tmp-dir']['value'], "Temporary"):
+            return
+        if not self.verify_safe2delete(EZVARS_aux['vert-sti']['output-dir']['value'], "Output"):
+            return
         
         # if self.validate_input_structure_1set() or self.validate_row_entries():
         #     return

commit e4e04461ce14da8e7bed1095a147a598e966b40b

ez: ez-stitch cleaner implementation of verify_safe2delete
Fixes a bug introduced in cf9a6e4dc

tofu/ez/GUI/Stitch_tools_tab/ezstitch_qt.py
@@ -593,20 +593,22 @@ class EZStitchGroup(QGroupBox):
                     rmtree(dir_path)
                 except:
                     warning_message(f"Error while deleting {dir_type} directory")
-                    return False
-                else:
-                    return True
+                    raise FileExistsError
             else:
-                return False
+                raise FileExistsError
 
 
 
     def stitch_button_pressed(self):
         LOG.debug("Stitch button pressed")
 
-        if not self.verify_safe2delete(EZVARS_aux['vert-sti']['tmp-dir']['value'], "Temporary"):
+        try:
+            self.verify_safe2delete(EZVARS_aux['vert-sti']['tmp-dir']['value'], "Temporary")
+        except FileExistsError:
             return
-        if not self.verify_safe2delete(EZVARS_aux['vert-sti']['output-dir']['value'], "Output"):
+        try:
+            self.verify_safe2delete(EZVARS_aux['vert-sti']['output-dir']['value'], "Output")
+        except FileExistsError:
             return
         
         # if self.validate_input_structure_1set() or self.validate_row_entries():